81f12d9b47d9e73d3258fcb9ebf29c0318caea1e
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-10 16:01:26 +0200

    docs: enhance comments for `Number` nterfaces

--- assert/internal/number.go
+++ assert/internal/number.go
@@ -4,18 +4,25 @@ import (
 	"reflect"
 )
 
+// Number represents any built-in numeric type supported by this package.
+//
+// It includes all signed and unsigned integer types, uintptr, and
+// floating-point types.
 type Number interface {
 	Signed | Unsigned | Float
 }
 
+// Signed represents any built-in signed integer type.
 type Signed interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
+// Unsigned represents any built-in unsigned integer type, including uintptr.
 type Unsigned interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
+// Float represents any built-in floating-point type.
 type Float interface {
 	~float32 | ~float64
 }
