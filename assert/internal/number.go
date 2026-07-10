91da811dab559f4bc1b280bf41bfbee2be85bb62
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-10 16:00:12 +0200

    refactor: restructure Number interface

--- assert/internal/number.go
+++ assert/internal/number.go
@@ -5,9 +5,19 @@ import (
 )
 
 type Number interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-		~float32 | ~float64
+	Signed | Unsigned | Float
+}
+
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Float interface {
+	~float32 | ~float64
 }
 
 func ToNumber[T Number](want T, have any) (T, bool) {
